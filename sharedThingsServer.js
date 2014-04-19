bdd86103fc43f215d66bac0b3715b78a7073fb02
tang <tangning111@gmail.com>
2014-04-19 21:20:51 +0800

    json log client ws

--- sharedThingsServer.js
+++ sharedThingsServer.js
@@ -55,7 +55,7 @@ wss.on('connection', function(ws) {
 	sendWorldsNameForNewClient(ws);
 	sendIndexForNewClient(ws);
 
-	console.log("client id is " + ws.id);
+	console.log(JSON.stringify(ws));
 
 	ws.on('message', function(message) {
 		var request = JSON.parse(message);
